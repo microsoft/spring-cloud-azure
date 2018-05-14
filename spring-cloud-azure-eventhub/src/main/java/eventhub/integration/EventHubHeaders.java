80dcd9cb79c0134455689ff3af13ce6cfd1edb30
Warren Zhu <zhonzh@microsoft.com>
2018-05-14 15:30:48 +0800

    Add EventHubHeaders for internal headers such as partiton id or key

---
+++ spring-cloud-azure-eventhub/src/main/java/eventhub/integration/EventHubHeaders.java
@@ -0,0 +1,31 @@
+/*
+ *  Copyright 2017 original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package eventhub.integration;
+
+/**
+ * Azure event hub internal headers for Spring Messaging messages.
+ *
+ * @author Warren Zhu
+ */
+public class EventHubHeaders {
+
+    private static final String PREFIX = "azure_event_hub_";
+
+    public static final String PARTITION_ID = PREFIX + "partition_id";
+
+    public static final String PARTITION_KEY = PREFIX + "partition_key";
+}
