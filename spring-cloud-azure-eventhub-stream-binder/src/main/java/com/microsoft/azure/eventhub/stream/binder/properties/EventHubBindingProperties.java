85f26bfa1583567360b50f33eb055a0b13d5289d
Warren Zhu <zhonzh@microsoft.com>
2018-06-04 17:36:24 +0800

    Implement Spring cloud stream event hub binder

---
+++ spring-cloud-azure-eventhub-stream-binder/src/main/java/com/microsoft/azure/eventhub/stream/binder/properties/EventHubBindingProperties.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.eventhub.stream.binder.properties;
+
+/**
+ * @author Warren Zhu
+ */
+public class EventHubBindingProperties {
+    private EventHubConsumerProperties consumer = new EventHubConsumerProperties();
+    private EventHubProducerProperties producer = new EventHubProducerProperties();
+
+    public EventHubConsumerProperties getConsumer() {
+        return this.consumer;
+    }
+
+    public void setConsumer(EventHubConsumerProperties consumer) {
+        this.consumer = consumer;
+    }
+
+    public EventHubProducerProperties getProducer() {
+        return this.producer;
+    }
+
+    public void setProducer(EventHubProducerProperties producer) {
+        this.producer = producer;
+    }
+}
