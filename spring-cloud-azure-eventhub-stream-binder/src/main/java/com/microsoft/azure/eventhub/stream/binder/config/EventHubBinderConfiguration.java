4e97a91657c5b59978897c2da41599d477785bef
Warren Zhu <zhonzh@microsoft.com>
2018-07-12 15:07:59 +0800

    Add telemetry in event hub binder configuration

--- spring-cloud-azure-eventhub-stream-binder/src/main/java/com/microsoft/azure/eventhub/stream/binder/config/EventHubBinderConfiguration.java
+++ spring-cloud-azure-eventhub-stream-binder/src/main/java/com/microsoft/azure/eventhub/stream/binder/config/EventHubBinderConfiguration.java
@@ -9,24 +9,42 @@ package com.microsoft.azure.eventhub.stream.binder.config;
 import com.microsoft.azure.eventhub.stream.binder.EventHubMessageChannelBinder;
 import com.microsoft.azure.eventhub.stream.binder.properties.EventHubExtendedBindingProperties;
 import com.microsoft.azure.eventhub.stream.binder.provisioning.EventHubChannelProvisioner;
+import com.microsoft.azure.spring.cloud.autoconfigure.context.AzureContextAutoConfiguration;
+import com.microsoft.azure.spring.cloud.autoconfigure.telemetry.TelemetryAutoConfiguration;
+import com.microsoft.azure.spring.cloud.autoconfigure.telemetry.TelemetryTracker;
 import com.microsoft.azure.spring.cloud.context.core.AzureAdmin;
 import com.microsoft.azure.spring.integration.eventhub.DefaultEventHubClientFactory;
 import com.microsoft.azure.spring.integration.eventhub.EventHubClientFactory;
 import com.microsoft.azure.spring.integration.eventhub.EventHubOperation;
 import com.microsoft.azure.spring.integration.eventhub.EventHubTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.stream.binder.Binder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 /**
  * @author Warren Zhu
  */
 @Configuration
 @ConditionalOnMissingBean(Binder.class)
+@AutoConfigureAfter({AzureContextAutoConfiguration.class, TelemetryAutoConfiguration.class})
 @EnableConfigurationProperties(EventHubExtendedBindingProperties.class)
 public class EventHubBinderConfiguration {
+
+    private static final String EVENT_HUB_BINDER = "EventHubBinder";
+
+    @Autowired(required = false)
+    private TelemetryTracker telemetryTracker;
+
+    @PostConstruct
+    public void triggerTelemetry() {
+        TelemetryTracker.triggerEvent(telemetryTracker, EVENT_HUB_BINDER);
+    }
 
     @Bean
     public EventHubClientFactory clientFactory(AzureAdmin azureAdmin,
