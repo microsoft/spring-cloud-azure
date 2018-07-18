68d74b6f167ac1712790667f22666a1b5980323d
weiping <wepa@microsoft.com>
2018-07-18 16:15:42 +0800

    show CloudError code and body

--- spring-cloud-azure-context/src/main/java/com/microsoft/azure/spring/cloud/context/core/ResourceCreatingAspect.java
+++ spring-cloud-azure-context/src/main/java/com/microsoft/azure/spring/cloud/context/core/ResourceCreatingAspect.java
@@ -52,9 +52,10 @@ public class ResourceCreatingAspect {
         try {
             return joinPoint.proceed();
         } catch (CloudException e) {
-            LOG.error("{} with name '{}' failed due to: {}", methodName, name, e.getMessage());
+            String errorMessage = String.join(", ", e.getMessage(), e.body().code(), e.body().message());
+            LOG.error("{} with name '{}' failed due to: {}", methodName, name, errorMessage);
             throw new RuntimeException(
-                    String.format("%s with name '%s' failed due to: %s", methodName, name, e.getMessage()));
+                    String.format("%s with name '%s' failed due to: %s", methodName, name, errorMessage));
         } finally {
             stopWatch.stop();
             LOG.info("{} with name '{}' finished in {} seconds", methodName, name, stopWatch.getTotalTimeSeconds());
