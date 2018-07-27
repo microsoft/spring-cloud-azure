4d4cf1a7888d1d2a59c02f559d8007fed79f33ed
miaosakurai <867616074@qq.com>
2018-07-27 15:40:57 +0800

    Fix checkstyle of cosmosdb-mongodb-sample.

--- spring-cloud-azure-samples/spring-cloud-azure-cosmosdb-mongodb-sample/src/main/java/com/example/User.java
+++ spring-cloud-azure-samples/spring-cloud-azure-cosmosdb-mongodb-sample/src/main/java/com/example/User.java
@@ -1,14 +1,21 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
 package com.example;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
+@Getter
+@Setter
 @Document
 public class User {
     @Id
     private String id;
     private String name;
-
 }
--- spring-cloud-azure-samples/spring-cloud-azure-cosmosdb-mongodb-sample/src/main/java/com/example/WebController.java
+++ spring-cloud-azure-samples/spring-cloud-azure-cosmosdb-mongodb-sample/src/main/java/com/example/WebController.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
 package com.example;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -5,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
@@ -13,19 +18,19 @@ public class WebController {
 
     private final MongoTemplate mongoTemplate;
 
-    WebController(MongoTemplate mongoTemplate){
-        this.mongoTemplate=mongoTemplate;
+    WebController(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
     }
 
     @PostMapping("/insertUser/{name}")
-    void insertUser(@PathVariable String name){
-        User user=new User();
+    void insertUser(@PathVariable String name) {
+        User user = new User();
         user.setName(name);
-        this.mongoTemplate.insert(user,"user");
+        this.mongoTemplate.insert(user, "user");
     }
 
     @GetMapping("/users")
-    List<User> findUsers(){
-        return (List<User>) mongoTemplate.findAll(User.class);
+    List<User> findUsers() {
+        return mongoTemplate.findAll(User.class);
     }
 }
