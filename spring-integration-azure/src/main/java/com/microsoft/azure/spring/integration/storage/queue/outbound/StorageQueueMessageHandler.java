dbbcccdd7b311079fa4c087e3853d1ed5fedef27
miaosakurai <867616074@qq.com>
2018-08-10 11:47:02 +0800

    Add storage queue support to spring integration azure

---
+++ spring-integration-azure/src/main/java/com/microsoft/azure/spring/integration/storage/queue/outbound/StorageQueueMessageHandler.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.spring.integration.storage.queue.outbound;
+
+import com.microsoft.azure.spring.integration.core.AbstractAzureMessageHandler;
+import com.microsoft.azure.spring.integration.core.SendOperation;
+import com.microsoft.azure.storage.queue.CloudQueueMessage;
+import org.springframework.messaging.Message;
+
+public class StorageQueueMessageHandler extends AbstractAzureMessageHandler<CloudQueueMessage> {
+
+    public StorageQueueMessageHandler(String destination, SendOperation<CloudQueueMessage> sendOperation) {
+        super(destination, sendOperation);
+    }
+
+    @Override
+    public CloudQueueMessage toAzureMessage(Message<?> message) {
+        Object payload  = message.getPayload();
+        if (payload instanceof CloudQueueMessage) {
+            return (CloudQueueMessage) payload;
+        } else if (payload instanceof String) {
+            return new CloudQueueMessage((String) payload);
+        } else if (payload instanceof byte[]) {
+            return new CloudQueueMessage((byte[]) payload);
+        } else {
+            return new CloudQueueMessage(String.valueOf(payload));
+        }
+    }
+}
