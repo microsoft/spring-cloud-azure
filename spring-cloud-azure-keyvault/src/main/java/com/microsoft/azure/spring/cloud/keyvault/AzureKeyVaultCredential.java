3ae8cd5a3c30768ae2d8c853064c4998b7e8797a
Warren Zhu <zhonzh@microsoft.com>
2018-08-16 17:11:00 +0800

    Add Key Vault as property source

---
+++ spring-cloud-azure-keyvault/src/main/java/com/microsoft/azure/spring/cloud/keyvault/AzureKeyVaultCredential.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.spring.cloud.keyvault;
+
+import com.microsoft.aad.adal4j.AuthenticationContext;
+import com.microsoft.aad.adal4j.AuthenticationResult;
+import com.microsoft.aad.adal4j.ClientCredential;
+import com.microsoft.azure.keyvault.authentication.KeyVaultCredentials;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Credential used for Key Vault call. This class just fetch token by provided
+ * {@code clientId} and {@code clientKey}. This should be provided by Azure SDK
+ *
+ * @author Warren Zhu
+ */
+@AllArgsConstructor
+public class AzureKeyVaultCredential extends KeyVaultCredentials {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AzureKeyVaultCredential.class);
+    private final String clientId;
+    private final String clientKey;
+
+    @Override
+    public String doAuthenticate(String authorization, String resource, String scope) {
+        try {
+            AuthenticationContext context =
+                    new AuthenticationContext(authorization, false, Executors.newSingleThreadExecutor());
+
+            Future<AuthenticationResult> future =
+                    context.acquireToken(resource, new ClientCredential(this.clientId, this.clientKey), null);
+            return future.get().getAccessToken();
+        } catch (MalformedURLException | InterruptedException | ExecutionException e) {
+            LOGGER.error("Failed to do Azure Key Vault authentication.", e);
+            throw new IllegalStateException("Failed to do Azure Key Vault authentication.", e);
+        }
+    }
+}
---
+++ spring-cloud-azure-keyvault/src/main/java/com/microsoft/azure/spring/cloud/keyvault/KeyVaultTemplate.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.spring.cloud.keyvault;
+
+import com.microsoft.azure.AzureResponseBuilder;
+import com.microsoft.azure.PagedList;
+import com.microsoft.azure.keyvault.KeyVaultClient;
+import com.microsoft.azure.keyvault.models.SecretItem;
+import com.microsoft.azure.serializer.AzureJacksonAdapter;
+import com.microsoft.azure.spring.cloud.context.core.Memoizer;
+import com.microsoft.azure.spring.cloud.context.core.Tuple;
+import com.microsoft.rest.RestClient;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Default implementation of {@link KeyVaultOperation}
+ *
+ * @author Warren Zhu
+ */
+public class KeyVaultTemplate implements KeyVaultOperation {
+    private static final String KEY_VAULT_URL = "https://%s.vault.azure.net/";
+    private final Map<String, Set<String>> secretNamesByKeyVault = new ConcurrentHashMap<>();
+    private final Map<Tuple<String, String>, String> secretsByKeyVault = new ConcurrentHashMap<>();
+
+    private final AzureKeyVaultCredential credential;
+
+    private Function<String, KeyVaultClient> keyVaultClientCreator = keyVaultClientCreator();
+
+    public KeyVaultTemplate(String clientId, String clientKey) {
+        this.credential = new AzureKeyVaultCredential(clientId, clientKey);
+    }
+
+    private static String buildKeyVaultUrl(String keyVaultName) {
+        return String.format(KEY_VAULT_URL, keyVaultName);
+    }
+
+    @Override
+    public String getSecret(String keyVaultName, String secretName, boolean fromCache) {
+        Tuple<String, String> keyVaultAndSecret = Tuple.of(keyVaultName, secretName);
+        if (fromCache) {
+            this.secretsByKeyVault.computeIfAbsent(keyVaultAndSecret, this::fetchSecret);
+        } else {
+            this.secretsByKeyVault.put(keyVaultAndSecret, fetchSecret(keyVaultAndSecret));
+        }
+
+        return this.secretsByKeyVault.get(keyVaultAndSecret);
+    }
+
+    @Override
+    public Collection<String> listSecrets(String keyVaultName, boolean fromCache) {
+        if (fromCache) {
+            this.secretNamesByKeyVault.computeIfAbsent(keyVaultName, this::fetchSecretNames);
+        } else {
+            this.secretNamesByKeyVault.put(keyVaultName, fetchSecretNames(keyVaultName));
+        }
+
+        return this.secretNamesByKeyVault.get(keyVaultName);
+    }
+
+    private Set<String> fetchSecretNames(String keyVaultName) {
+        String keyVaultUrl = buildKeyVaultUrl(keyVaultName);
+        PagedList<SecretItem> secretItems = this.keyVaultClientCreator.apply(keyVaultName).listSecrets(keyVaultUrl);
+        return Collections.unmodifiableSet(
+                secretItems.stream().map(SecretItem::id).map(s -> s.substring(s.lastIndexOf('/') + 1))
+                           .collect(Collectors.toSet()));
+    }
+
+    private String fetchSecret(Tuple<String, String> keyVaultAndSecret) {
+        String keyVaultName = keyVaultAndSecret.getFirst();
+        return this.keyVaultClientCreator.apply(keyVaultName)
+                                         .getSecret(buildKeyVaultUrl(keyVaultName), keyVaultAndSecret.getSecond())
+                                         .value();
+    }
+
+    private KeyVaultClient buildKeyVaultClient(String keyVaultName) {
+        String keyVaultUrl = buildKeyVaultUrl(keyVaultName);
+        RestClient restClient = new RestClient.Builder().withBaseUrl(keyVaultUrl).withCredentials(this.credential)
+                                                        .withSerializerAdapter(new AzureJacksonAdapter())
+                                                        .withResponseBuilderFactory(new AzureResponseBuilder.Factory())
+                                                        .withUserAgent("").build();
+        return new KeyVaultClient(restClient);
+    }
+
+    private Function<String, KeyVaultClient> keyVaultClientCreator() {
+        return Memoizer.memoize(this::buildKeyVaultClient);
+    }
+
+}
