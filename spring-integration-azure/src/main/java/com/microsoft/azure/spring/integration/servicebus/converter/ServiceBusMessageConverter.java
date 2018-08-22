eb9226393261093dbae89392f6f685f1c2c9183f
Warren Zhu <zhonzh@microsoft.com>
2018-08-22 15:15:15 +0800

    Add ServiceBusMessageConverter

---
+++ spring-integration-azure/src/main/java/com/microsoft/azure/spring/integration/servicebus/converter/ServiceBusMessageConverter.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.spring.integration.servicebus.converter;
+
+import com.microsoft.azure.servicebus.IMessage;
+import com.microsoft.azure.servicebus.Message;
+import com.microsoft.azure.spring.integration.core.converter.AbstractAzureMessageConverter;
+import org.springframework.messaging.MessageHeaders;
+
+import java.util.UUID;
+
+/**
+ * A converter to turn a {@link org.springframework.messaging.Message} to {@link IMessage}
+ * and vice versa.
+ *
+ * @author Warren Zhu
+ */
+public class ServiceBusMessageConverter extends AbstractAzureMessageConverter<IMessage> {
+
+    @Override
+    protected byte[] getPayload(IMessage azureMessage) {
+        return azureMessage.getBody();
+    }
+
+    @Override
+    protected IMessage fromString(String payload) {
+        return new Message(payload);
+    }
+
+    @Override
+    protected IMessage fromByte(byte[] payload) {
+        return new Message(payload);
+    }
+
+    @Override
+    protected void setCustomHeaders(org.springframework.messaging.Message<?> message, IMessage serviceBusMessage) {
+        if (message.getHeaders().containsKey(MessageHeaders.CONTENT_TYPE)) {
+            serviceBusMessage.setContentType(message.getHeaders().get(MessageHeaders.CONTENT_TYPE, String.class));
+        }
+
+        if (message.getHeaders().containsKey(MessageHeaders.ID)) {
+            serviceBusMessage.setMessageId(message.getHeaders().get(MessageHeaders.ID, UUID.class).toString());
+        }
+    }
+}
