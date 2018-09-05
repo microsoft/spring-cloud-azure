480cd55310b110b3389d115290369247a50c5867
Warren Zhu <zhonzh@microsoft.com>
2018-09-05 14:37:00 +0800

    Add spring-cloud-azure-servicebus-topic-binder-sample

---
+++ spring-cloud-azure-samples/spring-cloud-azure-servicebus-topic-binder-sample/src/main/java/com/example/SinkExample.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+/**
+ * @author Warren Zhu
+ */
+@EnableBinding(Sink.class)
+public class SinkExample {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SinkExample.class);
+
+    @StreamListener(Sink.INPUT)
+    public void handleMessage(String message) {
+            LOGGER.info("New message received: " + message);
+    }
+}
