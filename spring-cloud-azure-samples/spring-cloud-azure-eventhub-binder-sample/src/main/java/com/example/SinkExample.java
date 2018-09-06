2d7d636a0d0b0f999dab24cb8af998d601cfda40
Warren Zhu <zhonzh@microsoft.com>
2018-09-06 17:28:56 +0800

    Add checkpoint logic in event hub binder sample

--- spring-cloud-azure-samples/spring-cloud-azure-eventhub-binder-sample/src/main/java/com/example/SinkExample.java
+++ spring-cloud-azure-samples/spring-cloud-azure-eventhub-binder-sample/src/main/java/com/example/SinkExample.java
@@ -6,11 +6,14 @@
 
 package com.example;
 
+import com.microsoft.azure.spring.integration.core.AzureHeaders;
+import com.microsoft.azure.spring.integration.core.api.Checkpointer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.handler.annotation.Header;
 
 /**
  * @author Warren Zhu
@@ -21,7 +24,13 @@ public class SinkExample {
     private static final Logger LOGGER = LoggerFactory.getLogger(SinkExample.class);
 
     @StreamListener(Sink.INPUT)
-    public void handleMessage(String message) {
-            LOGGER.info("New message received: " + message);
+    public void handleMessage(String message, @Header(AzureHeaders.CHECKPOINTER) Checkpointer checkpointer) {
+        LOGGER.info("New message received: '{}'", message);
+        checkpointer.success().handle((r, ex) -> {
+            if (ex == null) {
+                LOGGER.info("Message '{}' successfully checkpointed", message);
+            }
+            return null;
+        });
     }
 }
