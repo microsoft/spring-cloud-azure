ed294ab7c8faf37fa258fbb16d5b65767af8cca8
Warren Zhu <zhonzh@microsoft.com>
2018-09-07 09:06:21 +0800

    Add manual checkpoint support in service bus integration sample

--- spring-cloud-azure-samples/spring-cloud-azure-servicebus-integration-sample/src/main/java/example/TopicReceiveController.java
+++ spring-cloud-azure-samples/spring-cloud-azure-servicebus-integration-sample/src/main/java/example/TopicReceiveController.java
@@ -6,16 +6,20 @@
 
 package example;
 
-import com.microsoft.azure.spring.integration.eventhub.EventHubOperation;
-import com.microsoft.azure.spring.integration.eventhub.inbound.EventHubInboundChannelAdapter;
+import com.microsoft.azure.spring.integration.core.AzureHeaders;
+import com.microsoft.azure.spring.integration.core.api.CheckpointMode;
+import com.microsoft.azure.spring.integration.core.api.Checkpointer;
 import com.microsoft.azure.spring.integration.servicebus.inbound.ServiceBusTopicInboundChannelAdapter;
 import com.microsoft.azure.spring.integration.servicebus.topic.ServiceBusTopicOperation;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TopicReceiveController {
 
-    private static final Log LOGGER = LogFactory.getLog(TopicReceiveController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TopicReceiveController.class);
     private static final String INPUT_CHANNEL = "topic.input";
     private static final String TOPIC_NAME = "topic";
     private static final String SUBSCRIPTION_NAME = "group1";
@@ -33,13 +37,21 @@ public class TopicReceiveController {
      *  via {@link MessageChannel} has name {@value INPUT_CHANNEL}
      */
     @ServiceActivator(inputChannel = INPUT_CHANNEL)
-    public void messageReceiver(byte[] payload) {
-        LOGGER.info("Message arrived! Payload: " + new String(payload));
+    public void messageReceiver(byte[] payload, @Header(AzureHeaders.CHECKPOINTER) Checkpointer checkpointer) {
+        String message = new String(payload);
+        LOGGER.info("Message arrived! Payload: {}", message);
+        checkpointer.success().handle((r, ex) -> {
+            if (ex == null) {
+                LOGGER.info("Message '{}' successfully checkpointed", message);
+            }
+            return null;
+        });
     }
 
     @Bean
     public ServiceBusTopicInboundChannelAdapter topicMessageChannelAdapter(
             @Qualifier(INPUT_CHANNEL) MessageChannel inputChannel, ServiceBusTopicOperation topicOperation) {
+        topicOperation.setCheckpointMode(CheckpointMode.MANUAL);
         ServiceBusTopicInboundChannelAdapter adapter = new ServiceBusTopicInboundChannelAdapter(TOPIC_NAME,
                 topicOperation, SUBSCRIPTION_NAME);
         adapter.setOutputChannel(inputChannel);
