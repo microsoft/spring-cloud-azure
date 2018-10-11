9386b01a3bc78487e0abef264ed287c98a56c60d
Warren Zhu <zhonzh@microsoft.com>
2018-10-11 14:02:59 +0800

    Refactor storage queue operation using receive operation. Fix #270

--- spring-integration-azure/spring-integration-storage-queue/src/main/java/com/microsoft/azure/spring/integration/storage/queue/StorageQueueOperation.java
+++ spring-integration-azure/spring-integration-storage-queue/src/main/java/com/microsoft/azure/spring/integration/storage/queue/StorageQueueOperation.java
@@ -6,55 +6,23 @@
 
 package com.microsoft.azure.spring.integration.storage.queue;
 
-import com.microsoft.azure.spring.integration.core.api.CheckpointMode;
+import com.microsoft.azure.spring.integration.core.api.ReceiveOperation;
 import com.microsoft.azure.spring.integration.core.api.SendOperation;
-import org.springframework.messaging.Message;
-
-import java.util.concurrent.CompletableFuture;
 
 /**
  * Azure storage queue operation to support send and receive
  * {@link org.springframework.messaging.Message} asynchronously
  *
+ * You should checkpoint if message has been processed successfully, otherwise it will be visible again after certain
+ * time specified by {@link #setVisibilityTimeoutInSeconds(int)}.
+ *
  * @author Miao Cao
+ * @author Warren Zhu
  */
-public interface StorageQueueOperation extends SendOperation {
-
-    /**
-     * Receives a message from the head of queue. This message become invisible for a visibility timeout period.
-     * You can change visibility timeout by {@link #setVisibilityTimeoutInSeconds(int)}
-     * You should checkpoint if message has been processed successfully, otherwise it will be visible again.
-     */
-    CompletableFuture<Message<?>> receiveAsync(String queueName);
-
-    /**
-     * Receives a message from the head of queue. This message become invisible for a visibility timeout period.
-     * You should check point if message has been processed successfully, otherwise it will be visible again.
-     */
-    CompletableFuture<Message<?>> receiveAsync(String queueName, int visibilityTimeoutInSeconds);
-
-    int getVisibilityTimeoutInSeconds();
+public interface StorageQueueOperation extends SendOperation, ReceiveOperation {
 
     /**
      * Set visibility timeout. Default is 30
-     *
      */
     void setVisibilityTimeoutInSeconds(int visibilityTimeoutInSeconds);
-
-    CheckpointMode getCheckpointMode();
-
-    /**
-     * Set checkpoint mode. Default is {@link CheckpointMode#RECORD}
-     *
-     */
-    void setCheckpointMode(CheckpointMode checkpointMode);
-
-    Class<?> getMessagePayloadType();
-
-    /**
-     * Set payload type. Default is {@link byte[]}
-     *
-     * @param messagePayloadType Specifies the payload type of {@link Message}
-     */
-    void setMessagePayloadType(Class<?> messagePayloadType);
 }
