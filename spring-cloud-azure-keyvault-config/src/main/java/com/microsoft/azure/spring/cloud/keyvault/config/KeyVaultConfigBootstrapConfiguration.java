e9c679937f9c1c28cb41bcdd80a9f9f4964c8d02
Kevin Zhao <kevinzha@microsoft.com>
2018-10-16 14:07:47 +0800

    resolve comments

--- spring-cloud-azure-keyvault-config/src/main/java/com/microsoft/azure/spring/cloud/keyvault/config/KeyVaultConfigBootstrapConfiguration.java
+++ spring-cloud-azure-keyvault-config/src/main/java/com/microsoft/azure/spring/cloud/keyvault/config/KeyVaultConfigBootstrapConfiguration.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.Configuration;
 public class KeyVaultConfigBootstrapConfiguration {
 
     @Bean
-    KeyVaultPropertySourceLocator keyVaultPropertySourceLocator(KeyVaultConfigProperties properties) {
+    public KeyVaultPropertySourceLocator keyVaultPropertySourceLocator(KeyVaultConfigProperties properties) {
         return new KeyVaultPropertySourceLocator(properties);
     }
 }
