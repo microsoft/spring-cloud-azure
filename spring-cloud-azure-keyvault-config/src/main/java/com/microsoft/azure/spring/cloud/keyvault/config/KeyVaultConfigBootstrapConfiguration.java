ef2d70151c4e2906dcd61b838242e6d30b24c87a
Kevin Zhao <kvn.zhl@gmail.com>
2018-10-23 15:17:31 +0800

    Enable certificate and secret authentication with Key Vault (#290)

--- spring-cloud-azure-keyvault-config/src/main/java/com/microsoft/azure/spring/cloud/keyvault/config/KeyVaultConfigBootstrapConfiguration.java
+++ spring-cloud-azure-keyvault-config/src/main/java/com/microsoft/azure/spring/cloud/keyvault/config/KeyVaultConfigBootstrapConfiguration.java
@@ -6,21 +6,48 @@
 
 package com.microsoft.azure.spring.cloud.keyvault.config;
 
+import com.microsoft.azure.keyvault.KeyVaultClient;
+import com.microsoft.azure.spring.cloud.autoconfigure.telemetry.TelemetryAutoConfiguration;
+import com.microsoft.azure.spring.cloud.autoconfigure.telemetry.TelemetryCollector;
+import com.microsoft.azure.spring.cloud.keyvault.config.auth.*;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 /**
  * Spring Cloud Bootstrap Configuration for setting up an {@link KeyVaultPropertySourceLocator}.
  */
 @Configuration
-@EnableConfigurationProperties(KeyVaultConfigProperties.class)
+@AutoConfigureBefore(TelemetryAutoConfiguration.class)
 @ConditionalOnProperty(name = KeyVaultConfigProperties.ENABLED, matchIfMissing = true)
+@EnableConfigurationProperties(KeyVaultConfigProperties.class)
 public class KeyVaultConfigBootstrapConfiguration {
+    private static final String KEY_VAULT_CONFIG = "KeyVaultConfig";
+
+    @PostConstruct
+    public void collectTelemetry() {
+        TelemetryCollector.getInstance().addService(KEY_VAULT_CONFIG);
+    }
 
     @Bean
-    public KeyVaultPropertySourceLocator keyVaultPropertySourceLocator(KeyVaultConfigProperties properties) {
-        return new KeyVaultPropertySourceLocator(properties);
+    public KeyVaultClient keyVaultClient(KeyVaultConfigProperties properties, AuthenticationExecutorFactory factory) {
+        final Credentials credentials = properties.getCredentials();
+        AuthenticationExecutor executor = factory.create(credentials);
+        return new KeyVaultClient(new AadKeyVaultCredentials(executor));
+    }
+
+    @Bean
+    public KeyVaultPropertySourceLocator keyVaultPropertySourceLocator(KeyVaultClient client,
+                                                                       KeyVaultConfigProperties properties) {
+        return new KeyVaultPropertySourceLocator(client, properties);
+    }
+
+    @Bean
+    public AuthenticationExecutorFactory authenticationExecutorFactory() {
+        return new DefaultAuthenticationExecutorFactory();
     }
 }
--- spring-cloud-azure-keyvault-config/src/main/java/com/microsoft/azure/spring/cloud/keyvault/config/KeyVaultConfigProperties.java
+++ spring-cloud-azure-keyvault-config/src/main/java/com/microsoft/azure/spring/cloud/keyvault/config/KeyVaultConfigProperties.java
@@ -6,29 +6,35 @@
 
 package com.microsoft.azure.spring.cloud.keyvault.config;
 
+import com.microsoft.azure.spring.cloud.keyvault.config.auth.Credentials;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * Configuration properties for the Azure Key Vault integration with Spring Cloud Config.
  */
+@Getter
+@Setter
 @ConfigurationProperties(KeyVaultConfigProperties.CONFIG_PREFIX)
 @Validated
 public class KeyVaultConfigProperties {
     public static final String CONFIG_PREFIX = "spring.cloud.azure.keyvault.config";
     public static final String ENABLED = CONFIG_PREFIX + ".enabled";
 
-    @Getter @Setter
+    @NestedConfigurationProperty
+    @NotNull
+    private Credentials credentials;
+
     private boolean enabled = true;
 
-    @Getter @Setter
     private boolean failFast = true;
 
-    @Getter @Setter
     private String name;
 
-    @Getter @Setter
     private String activeProfile;
 }
