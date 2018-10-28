7f7ca17d6dce48bca6bf70fc757b9c78d8fdeffa
Ryan O'Meara <romeara@live.com>
2018-10-27 20:08:48 -0400

    GH-279 Remove unused imports and add generic specifications
    The build process is outputting several warnings, as described in
    GH-279. This changeset removes some the simpler warnings related to
    generic typing and unused imports.
    
    This change set does not by itself remove all warnings described in
    GH-279

--- spring-cloud-azure-samples/spring-cloud-azure-eventhub-kafka-sample/src/main/java/com/example/SourceExample.java
+++ spring-cloud-azure-samples/spring-cloud-azure-eventhub-kafka-sample/src/main/java/com/example/SourceExample.java
@@ -11,11 +11,8 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
 
 /**
  * @author Warren Zhu
