a09bc7ce28a8b5e8125ffcf44fe6363a35c5291f
Warren Zhu <zhonzh@microsoft.com>
2018-11-07 17:53:10 +0800

    Add spring messaging sample

---
+++ spring-cloud-azure-samples/azure-messaging-sample/src/main/java/com/example/User.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name='" + name + '\'' + '}';
+    }
+}
