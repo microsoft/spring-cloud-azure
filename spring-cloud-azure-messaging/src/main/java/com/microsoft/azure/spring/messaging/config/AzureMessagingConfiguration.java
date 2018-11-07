ab1d97e9c74f75a850c63b6673b7d6f924b73728
Warren Zhu <zhonzh@microsoft.com>
2018-11-07 17:47:49 +0800

    Add AzureMessagingConfiguration to provide ListenerContainerFactory

---
+++ spring-cloud-azure-messaging/src/main/java/com/microsoft/azure/spring/messaging/config/AzureMessagingConfiguration.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.spring.messaging.config;
+
+import com.microsoft.azure.spring.integration.core.api.SubscribeByGroupOperation;
+import com.microsoft.azure.spring.messaging.container.ListenerContainerFactory;
+import com.microsoft.azure.spring.messaging.container.DefaultAzureListenerContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Warren Zhu
+ */
+@Configuration
+public class AzureMessagingConfiguration {
+
+    @Bean
+    public ListenerContainerFactory azureListenerContainerFactory(SubscribeByGroupOperation subscribeByGroupOperation) {
+        return new DefaultAzureListenerContainerFactory(subscribeByGroupOperation);
+    }
+}
