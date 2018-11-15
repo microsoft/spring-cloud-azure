525c317a120ef5c6336ede145926a432da38ccf7
weiping <wepa@microsoft.com>
2018-11-15 14:11:27 +0800

    label query for null label

--- spring-cloud-azure-config/src/main/java/com/microsoft/azure/spring/cloud/config/AzureConfigPropertySource.java
+++ spring-cloud-azure-config/src/main/java/com/microsoft/azure/spring/cloud/config/AzureConfigPropertySource.java
@@ -42,11 +42,7 @@ public class AzureConfigPropertySource extends EnumerablePropertySource<ConfigSe
         List<KeyValueItem> items = source.getKeys(context + "*", label);
 
         for (KeyValueItem item : items) {
-            if (item.getLabel() != label) {
-                continue; // Skip non-expected label
-            }
-
-            String key = item.getKey().replaceFirst(context, "");
+            String key = item.getKey().trim().substring(context.length());
             properties.put(key, item.getValue());
         }
     }
