5fb7a53a7e1a0149f01c63cfe0768150fa2ca6f5
Warren Zhu <zhonzh@microsoft.com>
2018-11-26 14:19:28 +0800

    Fix wrong init count in EventHubCheckpointManager

--- spring-integration-azure/spring-integration-eventhub/src/main/java/com/microsoft/azure/spring/integration/eventhub/impl/EventHubCheckpointManager.java
+++ spring-integration-azure/spring-integration-eventhub/src/main/java/com/microsoft/azure/spring/integration/eventhub/impl/EventHubCheckpointManager.java
@@ -38,7 +38,7 @@ class EventHubCheckpointManager {
             context.checkpoint(eventData).whenComplete((v, t) -> checkpointHandler(context, eventData, t));
         } else if (this.checkpointConfig.getCheckpointMode() == CheckpointMode.PARTITION_COUNT) {
             String partitionId = context.getPartitionId();
-            this.countByPartition.computeIfAbsent(partitionId, (k) -> new AtomicInteger(1));
+            this.countByPartition.computeIfAbsent(partitionId, (k) -> new AtomicInteger(0));
             AtomicInteger count = this.countByPartition.get(partitionId);
             if (count.incrementAndGet() >= checkpointConfig.getCheckpointCount()) {
                 context.checkpoint(eventData).whenComplete((v, t) -> checkpointHandler(context, eventData, t));
