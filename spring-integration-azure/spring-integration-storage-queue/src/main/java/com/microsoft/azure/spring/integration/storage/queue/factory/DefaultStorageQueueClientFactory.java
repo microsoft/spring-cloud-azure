0aa1f4a3c1aa5c757de67d46248415d6583f5843
Warren Zhu <zhonzh@microsoft.com>
2018-11-29 16:22:07 +0800

    Cache CloudQueue in DefaultStorageQueueClientFactory

--- spring-integration-azure/spring-integration-storage-queue/src/main/java/com/microsoft/azure/spring/integration/storage/queue/factory/DefaultStorageQueueClientFactory.java
+++ spring-integration-azure/spring-integration-storage-queue/src/main/java/com/microsoft/azure/spring/integration/storage/queue/factory/DefaultStorageQueueClientFactory.java
@@ -7,12 +7,16 @@
 package com.microsoft.azure.spring.integration.storage.queue.factory;
 
 import com.microsoft.azure.spring.cloud.context.core.api.ResourceManager;
+import com.microsoft.azure.spring.cloud.context.core.util.Memoizer;
 import com.microsoft.azure.spring.cloud.context.core.util.Tuple;
 import com.microsoft.azure.storage.queue.CloudQueue;
 import org.springframework.lang.NonNull;
 
+import java.util.function.BiFunction;
+
 public class DefaultStorageQueueClientFactory implements StorageQueueClientFactory {
     private final ResourceManager<CloudQueue, Tuple<String, String>> storageQueueManager;
+    private final BiFunction<String, String, CloudQueue> cloudQueueCreator = Memoizer.memoize(this::createCloudQueue);
 
     public DefaultStorageQueueClientFactory(
             @NonNull ResourceManager<CloudQueue, Tuple<String, String>> storageQueueManager) {
@@ -21,6 +25,10 @@ public class DefaultStorageQueueClientFactory implements StorageQueueClientFacto
 
     @Override
     public CloudQueue getOrCreateQueueClient(String storageAccountName, String queueName) {
+        return this.cloudQueueCreator.apply(storageAccountName, queueName);
+    }
+
+    private CloudQueue createCloudQueue(String storageAccountName, String queueName){
         return storageQueueManager.getOrCreate(Tuple.of(storageAccountName, queueName));
     }
 }
