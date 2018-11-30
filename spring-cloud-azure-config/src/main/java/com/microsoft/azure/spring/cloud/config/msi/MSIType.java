933f96a264c72e7301ae31b058c019f81e9906d8
weiping <wepa@microsoft.com>
2018-11-30 13:56:10 +0800

    add MSI credential type

---
+++ spring-cloud-azure-config/src/main/java/com/microsoft/azure/spring/cloud/config/msi/MSIType.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.azure.spring.cloud.config.msi;
+
+public enum MSIType {
+    VM, APP_SERVICE
+}
