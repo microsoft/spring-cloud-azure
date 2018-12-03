d084e5202287bfda00619a67e5c3a591360b3e5e
weiping <wepa@microsoft.com>
2018-12-03 19:24:46 +0800

    ignore unknown

--- spring-cloud-azure-config/src/main/java/com/microsoft/azure/spring/cloud/config/msi/ConfigAcessKeys.java
+++ spring-cloud-azure-config/src/main/java/com/microsoft/azure/spring/cloud/config/msi/ConfigAcessKeys.java
@@ -5,6 +5,7 @@
  */
 package com.microsoft.azure.spring.cloud.config.msi;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,8 +24,8 @@ public class ConfigAcessKeys {
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 class ConfigAccessKey {
-    private String name;
     private String id;
     private String value;
     private boolean readOnly;
