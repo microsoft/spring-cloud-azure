8c72ecfdb85460590168274e349cc1aabeffcf21
Warren Zhu <zhonzh@microsoft.com>
2018-12-06 13:48:33 +0800

    Remove lombok in autoconfig module

--- spring-cloud-azure-autoconfigure/src/main/java/com/microsoft/azure/spring/cloud/autoconfigure/cache/AzureRedisProperties.java
+++ spring-cloud-azure-autoconfigure/src/main/java/com/microsoft/azure/spring/cloud/autoconfigure/cache/AzureRedisProperties.java
@@ -6,8 +6,6 @@
 
 package com.microsoft.azure.spring.cloud.autoconfigure.cache;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 import javax.validation.constraints.NotEmpty;
@@ -15,12 +13,18 @@ import javax.validation.constraints.NotEmpty;
 /**
  * @author Warren Zhu
  */
-@Getter
-@Setter
 @Validated
 @ConfigurationProperties("spring.cloud.azure.redis")
 public class AzureRedisProperties {
 
     @NotEmpty
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
