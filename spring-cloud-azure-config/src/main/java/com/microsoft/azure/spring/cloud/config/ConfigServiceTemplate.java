74fc26a7813ef73ecf0211503ca0c8850db6fa2d
weiping <wepa@microsoft.com>
2018-12-19 11:38:18 +0800

    keys will be searched from next page if next link header exists (#398)
    * keys will be searched from next page if next link header exists

--- spring-cloud-azure-config/src/main/java/com/microsoft/azure/spring/cloud/config/ConfigServiceTemplate.java
+++ spring-cloud-azure-config/src/main/java/com/microsoft/azure/spring/cloud/config/ConfigServiceTemplate.java
@@ -7,23 +7,33 @@ package com.microsoft.azure.spring.cloud.config;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.spring.cloud.config.domain.KeyValueItem;
 import com.microsoft.azure.spring.cloud.config.domain.KeyValueResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.Header;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
 
 @Slf4j
 @AllArgsConstructor
 public class ConfigServiceTemplate implements ConfigServiceOperations {
+    private static final String LINK_HEADER = "link";
+    private static final String NEXT_PAGE_LINK = "<(.*?)>; rel=\"next\".*";
+    private static final Pattern PAGE_LINK_PATTERN = Pattern.compile(NEXT_PAGE_LINK);
+
     public static final String LOAD_FAILURE_MSG = "Failed to load keys from Azure Config Service.";
     public static final String LOAD_FAILURE_VERBOSE_MSG = LOAD_FAILURE_MSG + " With status code: %s, reason: %s";
 
@@ -37,28 +47,77 @@ public class ConfigServiceTemplate implements ConfigServiceOperations {
     @Override
     public List<KeyValueItem> getKeys(@Nullable String prefix, @Nullable String label) {
         String requestUri = new RestAPIBuilder().withEndpoint(storeEndpoint).buildKVApi(prefix, label);
+        List<KeyValueItem> result = new ArrayList<>();
+
+        CloseableHttpResponse response = null;
+        try {
+            response = getRawResponse(requestUri);
+            while (response != null) {
+                try {
+                    KeyValueResponse kvResponse = mapper.readValue(response.getEntity().getContent(),
+                            KeyValueResponse.class);
+                    result.addAll(kvResponse.getItems());
+                } catch (IOException e) {
+                    throw new IllegalStateException(LOAD_FAILURE_MSG, e);
+                }
+
+                String nextLink = getNextLink(response);
+                if (!StringUtils.hasText(nextLink)) {
+                    break;
+                }
+
+                String nextRequestUri = new RestAPIBuilder().withEndpoint(storeEndpoint).withPath(nextLink)
+                        .buildKVApi();
+                response = getRawResponse(nextRequestUri);
+            }
+        } finally {
+            if (response != null) {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    log.warn("Response was not closed successfully.", e);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private CloseableHttpResponse getRawResponse(String requestUri) {
         HttpGet httpGet = new HttpGet(requestUri);
         Date date = new Date();
 
         log.debug("Loading key-value items from Azure Config service at [{}].", requestUri);
-        try (CloseableHttpResponse response = configClient.execute(httpGet, date, credential, secret)) {
+        try {
+            CloseableHttpResponse response = configClient.execute(httpGet, date, credential, secret);
             int statusCode = response.getStatusLine().getStatusCode();
 
             if (statusCode == HttpStatus.SC_OK) {
-                KeyValueResponse kvResponse = mapper.readValue(response.getEntity().getContent(),
-                        KeyValueResponse.class);
-
-                return kvResponse.getItems();
+                return response;
             } else {
                 throw new IllegalStateException(String.format(LOAD_FAILURE_VERBOSE_MSG, statusCode,
                         response.getStatusLine().getReasonPhrase()));
             }
-        }
-        catch (IOException | URISyntaxException e) {
+        } catch (IOException | URISyntaxException e) {
             log.error(LOAD_FAILURE_MSG, e);
             // TODO (wp) wrap exception as config service specific exception in order to provide fail-fast etc.
             // features?
             throw new IllegalStateException(LOAD_FAILURE_MSG, e);
         }
+    }
+
+    /**
+     * Extract next link path and query from {@code response} header
+     * @param response which response to extract link header from
+     * @return matched next link header, return empty string if not found
+     */
+    private static String getNextLink(@NonNull CloseableHttpResponse response) {
+        Header linkHeader = response.getFirstHeader(LINK_HEADER);
+        if (linkHeader == null || !StringUtils.hasText(linkHeader.getValue())) {
+            return "";
+        }
+
+        Matcher linkMatcher = PAGE_LINK_PATTERN.matcher(linkHeader.getValue());
+        return linkMatcher.matches() ? linkMatcher.group(1) : "";
     }
 }
--- spring-cloud-azure-config/src/main/java/com/microsoft/azure/spring/cloud/config/RestAPIBuilder.java
+++ spring-cloud-azure-config/src/main/java/com/microsoft/azure/spring/cloud/config/RestAPIBuilder.java
@@ -70,6 +70,14 @@ public class RestAPIBuilder {
         return buildRequestUri();
     }
 
+    /**
+     * User is responsible to configure the required endpoint, path and params ahead
+     * @return valid full path of target REST API
+     */
+    public String buildKVApi() {
+        return buildRequestUri();
+    }
+
     private String buildRequestUri() {
         Assert.hasText(endpoint, "Endpoint should not be empty or null");
         Assert.hasText(path, "Request path should not be empty or null");
@@ -78,9 +86,11 @@ public class RestAPIBuilder {
         builder.append(endpoint);
         builder.append(path);
 
-        if (params != null && params.size() > 0) {
+        if (params != null && !params.isEmpty()) {
             // append query params, example: "?param1=value1&param2=value2"
-            builder.append("?");
+            if (!path.contains("?")) {
+                builder.append("?");
+            }
 
             String queryParams = String.join("&", params.entrySet().stream()
                     .map(p -> p.getKey() + "=" + p.getValue())
