af7bc7a4328f4bed2e66312fe3ac0391a71a7e5c
weiping <wepa@microsoft.com>
2019-01-09 17:06:48 +0800

    add config name to context

--- spring-cloud-azure-config/src/main/java/com/microsoft/azure/spring/cloud/config/AzureConfigPropertySource.java
+++ spring-cloud-azure-config/src/main/java/com/microsoft/azure/spring/cloud/config/AzureConfigPropertySource.java
@@ -19,7 +19,7 @@ public class AzureConfigPropertySource extends EnumerablePropertySource<ConfigSe
     private final ConfigStore configStore;
 
     public AzureConfigPropertySource(String context, ConfigServiceOperations operations, ConfigStore configStore) {
-        super(context, operations);
+        super(context + configStore.getName(), operations);
         this.context = context;
         this.configStore = configStore;
     }
