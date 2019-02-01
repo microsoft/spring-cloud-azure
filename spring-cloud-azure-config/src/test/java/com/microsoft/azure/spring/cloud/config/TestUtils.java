fbe5312ae14a76d14eab6f59fdfbb368c78bf671
weiping <wepa@microsoft.com>
2019-02-01 14:22:57 +0800

    Implement operations for the azure config service

---
+++ spring-cloud-azure-config/src/test/java/com/microsoft/azure/spring/cloud/config/TestUtils.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.azure.spring.cloud.config;
+
+import com.microsoft.azure.spring.cloud.config.domain.KeyValueItem;
+
+import java.util.List;
+
+/**
+ * Utility methods which can be used across different test classes
+ */
+public class TestUtils {
+    private TestUtils() {
+    }
+
+    static String propPair(String propName, String propValue) {
+        return String.format("%s=%s", propName, propValue);
+    }
+
+    static KeyValueItem createItem(String context, String key, String value, String label) {
+        KeyValueItem item = new KeyValueItem();
+        item.setKey(context + key);
+        item.setValue(value);
+        item.setLabel(label);
+
+        return item;
+    }
+
+    static void addStore(AzureCloudConfigProperties properties, String storeName, String connectionString) {
+        addStore(properties, storeName, connectionString, null);
+    }
+
+    static void addStore(AzureCloudConfigProperties properties, String storeName, String connectionString,
+                         String label) {
+        List<ConfigStore> stores = properties.getStores();
+        ConfigStore store = new ConfigStore();
+        store.setConnectionString(connectionString);
+        store.setName(storeName);
+        store.setLabel(label);
+        stores.add(store);
+        properties.setStores(stores);
+    }
+}
