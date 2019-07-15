976f92ab9f98b03409399e5533cb27ff7f108c29
junjie-x <xiejj365@foxmail.com>
2019-07-15 08:01:05 +0800

    Add test for jms sample

---
+++ spring-cloud-azure-samples/servicebus-jms-sample/src/test/java/com/example/ServiceBusJMSApplicationIT.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ServiceBusJMSApplicationIT {
+
+    @Test
+    public void testJMSSendAndReceiveMessage() {
+
+        Assert.assertEquals(ReceiveController.getContent(), ServiceBusJMSApplication.getContent());
+
+    }
+
+}
