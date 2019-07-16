dda1cf85974af3e1b53f2d71280083ca8bfacfc7
junjie-x <xiejj365@foxmail.com>
2019-07-16 10:51:10 +0800

    Modified jms sample

--- spring-cloud-azure-autoconfigure/src/main/java/com/microsoft/azure/spring/cloud/autoconfigure/jms/ServiceBusKey.java
+++ spring-cloud-azure-autoconfigure/src/main/java/com/microsoft/azure/spring/cloud/autoconfigure/jms/ServiceBusKey.java
@@ -11,27 +11,16 @@ public class ServiceBusKey {
         this.sharedAccessKey = sharedAccessKey;
     }
 
-//    public void setHost(String host) {
-//        this.host = host;
-//    }
-
     public String getHost() {
         return host;
     }
-
-//    public void setSharedAccessKeyName(String sharedAccessKeyName) {
-//        this.sharedAccessKeyName = sharedAccessKeyName;
-//    }
 
     public String getSharedAccessKeyName() {
         return sharedAccessKeyName;
     }
 
-//    public void setSharedAccessKey(String sharedAccessKey) {
-//        this.sharedAccessKey = sharedAccessKey;
-//    }
-
     public String getSharedAccessKey() {
         return sharedAccessKey;
     }
+
 }
--- spring-cloud-azure-samples/servicebus-jms-sample/src/main/java/com/example/ServiceBusJMSApplication.java
+++ spring-cloud-azure-samples/servicebus-jms-sample/src/main/java/com/example/ServiceBusJMSApplication.java
@@ -6,65 +6,18 @@
 
 package com.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication
 @EnableJms
 public class ServiceBusJMSApplication {
 
-    private static final String QUEUE_NAME = "que001";
-
-    private static final String USER_NAME = "usr001";
-
-    private static final Logger logger = LoggerFactory.getLogger(ServiceBusJMSApplication.class);
-
-    static ConfigurableApplicationContext context;
-
-    @Autowired
-    private static JmsTemplate jmsTemplate;
-
     public static void main(String[] args) {
 
-        context = SpringApplication.run(ServiceBusJMSApplication.class, args);
-
-        jmsTemplate = context.getBean(JmsTemplate.class);
-
-        logger.info("Sending message");
-
-        jmsTemplate.convertAndSend(QUEUE_NAME, new User(USER_NAME));
+        SpringApplication.run(ServiceBusJMSApplication.class, args);
 
     }
 
 }
-
-//package com.example;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.ConfigurableApplicationContext;
-//import org.springframework.jms.annotation.EnableJms;
-//import org.springframework.jms.core.JmsTemplate;
-//
-//@SpringBootApplication
-//@EnableJms
-//public class ServiceBusJMSApplication {
-//
-//    static ConfigurableApplicationContext context;
-//
-//    public static void main(String[] args) {
-//
-//        context = SpringApplication.run(ServiceBusJMSApplication.class, args);
-//
-//    }
-//
-//}
