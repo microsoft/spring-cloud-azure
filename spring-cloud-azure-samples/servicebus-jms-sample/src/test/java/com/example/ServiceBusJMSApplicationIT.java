f0b788d207cf5efc1b4d2231fb8cd4fba6ac91b7
junjie-x <xiejj365@foxmail.com>
2019-07-16 13:49:17 +0800

    Add IT test for jms sample

--- spring-cloud-azure-samples/servicebus-jms-sample/src/test/java/com/example/ServiceBusJMSApplicationIT.java
+++ spring-cloud-azure-samples/servicebus-jms-sample/src/test/java/com/example/ServiceBusJMSApplicationIT.java
@@ -4,18 +4,63 @@
  * license information.
  */
 
-//package com.example;
+package com.example;
 
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class ServiceBusJMSApplicationIT {
-//
-//    @Test
-//    public void testJMSSendAndReceiveMessage() {
-//
-//        Assert.assertEquals(ReceiveController.getContent(), ServiceBusJMSApplication.getContent());
-//
-//    }
-//
-//}
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.rule.OutputCapture;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServiceBusJMSApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class ServiceBusJMSApplicationIT {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Rule
+    public OutputCapture capture = new OutputCapture();
+
+    @Test
+    public void testSendAndReceiveMessage() throws Exception {
+        String message = UUID.randomUUID().toString();
+
+        mvc.perform(post("/messages?message=" + message)).andExpect(status().isOk())
+                .andExpect(content().string(message));
+
+        String messageReceivedLog = String.format("Receiving message from: %s", message);
+
+        boolean messageReceived = false;
+
+        for (int i = 0; i < 100; i++) {
+            String output = capture.toString();
+            if (!messageReceived && output.contains(messageReceivedLog)) {
+                messageReceived = true;
+            }
+
+            if (messageReceived) {
+                break;
+            }
+
+            Thread.sleep(1000);
+        }
+
+        assertThat(messageReceived).isTrue();
+    }
+
+}
