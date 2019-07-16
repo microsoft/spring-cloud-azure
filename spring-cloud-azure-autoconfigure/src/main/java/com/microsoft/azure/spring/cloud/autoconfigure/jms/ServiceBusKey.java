71eda70c37138d636d10459f7b5a6512cb59db90
junjie-x <xiejj365@foxmail.com>
2019-07-16 09:45:22 +0800

    Modified ServiceBusKey and ConnectionStringResolver

--- spring-cloud-azure-autoconfigure/src/main/java/com/microsoft/azure/spring/cloud/autoconfigure/jms/ServiceBusKey.java
+++ spring-cloud-azure-autoconfigure/src/main/java/com/microsoft/azure/spring/cloud/autoconfigure/jms/ServiceBusKey.java
@@ -1,32 +1,35 @@
 package com.microsoft.azure.spring.cloud.autoconfigure.jms;
 
 public class ServiceBusKey {
-    private String host;
-    private String sharedAccessKeyName;
-    private String sharedAccessKey;
+    private final String host;
+    private final String sharedAccessKeyName;
+    private final String sharedAccessKey;
 
-    ServiceBusKey() {
-    }
-
-    public void setHost(String host) {
+    ServiceBusKey(String host, String sharedAccessKeyName, String sharedAccessKey) {
         this.host = host;
+        this.sharedAccessKeyName = sharedAccessKeyName;
+        this.sharedAccessKey = sharedAccessKey;
     }
+
+//    public void setHost(String host) {
+//        this.host = host;
+//    }
 
     public String getHost() {
         return host;
     }
 
-    public void setSharedAccessKeyName(String sharedAccessKeyName) {
-        this.sharedAccessKeyName = sharedAccessKeyName;
-    }
+//    public void setSharedAccessKeyName(String sharedAccessKeyName) {
+//        this.sharedAccessKeyName = sharedAccessKeyName;
+//    }
 
     public String getSharedAccessKeyName() {
         return sharedAccessKeyName;
     }
 
-    public void setSharedAccessKey(String sharedAccessKey) {
-        this.sharedAccessKey = sharedAccessKey;
-    }
+//    public void setSharedAccessKey(String sharedAccessKey) {
+//        this.sharedAccessKey = sharedAccessKey;
+//    }
 
     public String getSharedAccessKey() {
         return sharedAccessKey;
