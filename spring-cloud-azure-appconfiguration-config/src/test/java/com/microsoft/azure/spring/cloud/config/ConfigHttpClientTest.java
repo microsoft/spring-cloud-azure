e9f0d8ec97f5754d5edc9e50f8bcf90ef2ccd7c3
mrm9084 <mrm9084@gmail.com>
2019-07-19 13:29:45 -0700

    Fixing merge error of missing import.

--- spring-cloud-azure-appconfiguration-config/src/test/java/com/microsoft/azure/spring/cloud/config/ConfigHttpClientTest.java
+++ spring-cloud-azure-appconfiguration-config/src/test/java/com/microsoft/azure/spring/cloud/config/ConfigHttpClientTest.java
@@ -23,6 +23,7 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.TimeZone;
 import java.util.UUID;
