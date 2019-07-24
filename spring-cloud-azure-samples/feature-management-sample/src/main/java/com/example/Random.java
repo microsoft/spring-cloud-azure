392cb5d042bf7d14e65b116c2ee4496717a52d0b
mrm9084 <mrm9084@gmail.com>
2019-07-24 10:40:50 -0700

    added feature management samples and docs

---
+++ spring-cloud-azure-samples/feature-management-sample/src/main/java/com/example/Random.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.stereotype.Component;
+
+import com.microsoft.azure.spring.cloud.feature.manager.FeatureFilter;
+import com.microsoft.azure.spring.cloud.feature.manager.entities.FeatureFilterEvaluationContext;
+
+@Component("Random")
+public class Random implements FeatureFilter{
+
+    @Override
+    public boolean evaluate(FeatureFilterEvaluationContext context) {
+        double rand = Math.random();
+        int c = (int) context.getParameters().get("chance");
+        double chance = (double) c/100;
+        return rand > chance; 
+    }
+
+}
