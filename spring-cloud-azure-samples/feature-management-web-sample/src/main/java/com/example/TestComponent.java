dd1df929c8029f89dcf5e64602087f2e7ff64dc6
mrm9084 <mrm9084@gmail.com>
2019-07-24 10:40:50 -0700

    added feature management samples and docs

---
+++ spring-cloud-azure-samples/feature-management-web-sample/src/main/java/com/example/TestComponent.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.microsoft.azure.spring.cloud.feature.manager.FeatureManager;
+
+@Component
+public class TestComponent {
+    
+    @Autowired
+    FeatureManager featureManager;
+    
+    public String test() {
+        if(featureManager.isEnabled("Beta")) {
+            return "Beta";
+        }
+        return "Original";
+    }
+
+}
