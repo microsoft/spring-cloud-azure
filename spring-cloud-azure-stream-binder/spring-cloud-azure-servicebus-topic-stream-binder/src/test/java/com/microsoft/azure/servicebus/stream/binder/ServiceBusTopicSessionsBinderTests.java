794251a7d4f62fd2bdc1b70a53f7ed0cd08085b7
esciullo <eduardo.sciullo@alliance-healthcare.net>
2019-11-14 17:48:14 +0100

    Test for consumers with sessionEnabled

--- spring-cloud-azure-stream-binder/spring-cloud-azure-servicebus-topic-stream-binder/src/test/java/com/microsoft/azure/servicebus/stream/binder/ServiceBusTopicSessionsBinderTests.java
+++ spring-cloud-azure-stream-binder/spring-cloud-azure-servicebus-topic-stream-binder/src/test/java/com/microsoft/azure/servicebus/stream/binder/ServiceBusTopicSessionsBinderTests.java
@@ -6,25 +6,12 @@
 
 package com.microsoft.azure.servicebus.stream.binder;
 
-import com.microsoft.azure.servicebus.SubscriptionClient;
 import com.microsoft.azure.servicebus.stream.binder.properties.ServiceBusConsumerProperties;
-import com.microsoft.azure.servicebus.stream.binder.properties.ServiceBusProducerProperties;
-import com.microsoft.azure.servicebus.stream.binder.test.AzurePartitionBinderTests;
-import com.microsoft.azure.spring.integration.servicebus.factory.ServiceBusTopicClientFactory;
-import com.microsoft.azure.spring.integration.servicebus.topic.support.ServiceBusTopicTestOperation;
-import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
 import org.springframework.cloud.stream.binder.ExtendedConsumerProperties;
-import org.springframework.cloud.stream.binder.ExtendedProducerProperties;
 import org.springframework.cloud.stream.binder.HeaderMode;
-
-import java.util.concurrent.CompletableFuture;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 /**
  * Test cases are defined in super class
@@ -33,63 +20,18 @@ import static org.mockito.Mockito.when;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class ServiceBusTopicSessionsBinderTests extends
-        AzurePartitionBinderTests<ServiceBusTopicTestBinder, ExtendedConsumerProperties<ServiceBusConsumerProperties>,
-                ExtendedProducerProperties<ServiceBusProducerProperties>> {
-    @Mock
-    ServiceBusTopicClientFactory clientFactory;
-
-    @Mock
-    SubscriptionClient subscriptionClient;
-
-    private ServiceBusTopicTestBinder binder;
-
-    @Before
-    public void setUp() {
-        when(this.clientFactory.getOrCreateSubscriptionClient(anyString(), anyString()))
-                .thenReturn(this.subscriptionClient);
-        CompletableFuture<Void> future = new CompletableFuture<>();
-        future.complete(null);
-        when(this.subscriptionClient.completeAsync(any())).thenReturn(future);
-        this.binder = new ServiceBusTopicTestBinder(new ServiceBusTopicTestOperation(this.clientFactory));
-    }
-
-    @Override
-    protected String getClassUnderTestName() {
-        return ServiceBusTopicTestBinder.class.getSimpleName();
-    }
-
-    @Override
-    protected ServiceBusTopicTestBinder getBinder() throws Exception {
-        return this.binder;
-    }
+        ServiceBusTopicPartitionBinderTests {
 
     @Override
     protected ExtendedConsumerProperties<ServiceBusConsumerProperties> createConsumerProperties() {
-        
-        ServiceBusConsumerProperties serviceBusConsumerProperties =  new ServiceBusConsumerProperties();
+
+        ServiceBusConsumerProperties serviceBusConsumerProperties = new ServiceBusConsumerProperties();
         serviceBusConsumerProperties.setSessionEnabled(true);
-        
-        ExtendedConsumerProperties<ServiceBusConsumerProperties> properties =
-                new ExtendedConsumerProperties<>(serviceBusConsumerProperties);
+
+        ExtendedConsumerProperties<ServiceBusConsumerProperties> properties = new ExtendedConsumerProperties<>(
+                serviceBusConsumerProperties);
         properties.setHeaderMode(HeaderMode.embeddedHeaders);
         return properties;
     }
 
-    @Override
-    protected ExtendedProducerProperties<ServiceBusProducerProperties> createProducerProperties() {
-        ExtendedProducerProperties<ServiceBusProducerProperties> properties =
-                new ExtendedProducerProperties<>(new ServiceBusProducerProperties());
-        properties.setHeaderMode(HeaderMode.embeddedHeaders);
-        return properties;
-    }
-
-    @Override
-    public void testOneRequiredGroup() {
-        // Required group test rely on unsupported start position of consumer properties
-    }
-
-    @Override
-    public void testTwoRequiredGroups() {
-        // Required group test rely on unsupported start position of consumer properties
-    }
 }
