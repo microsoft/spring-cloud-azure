6199fce892534ca9aa428e6c3f23b237572cd083
Xiaolu Dai <xiada@microsoft.com>
2020-04-01 15:35:03 +0800

    Prepend application id to User-Agent header

---
+++ spring-cloud-azure-context/src/main/java/com/microsoft/azure/spring/cloud/context/core/util/Constants.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.azure.spring.cloud.context.core.util;
+
+public class Constants {
+
+    // for the User-Agent header set in track2 SDKs
+    public static final String SPRINGCLOUD_VERSION = "1.1.3-SNAPSHOT";
+    public static final String SPRINGCLOUD_STORAGE_BLOB_APPLICATION_ID = "az-springcloud-storage-blob/"
+            + SPRINGCLOUD_VERSION;
+    public static final String SPRINGCLOUD_STORAGE_FILE_SHARE_APPLICATION_ID = "az-springcloud-storage-file-share/"
+            + SPRINGCLOUD_VERSION;
+    public static final String SPRINGCLOUD_STORAGE_QUEUE_APPLICATION_ID = "az-springcloud-storage-queue/"
+            + SPRINGCLOUD_VERSION;
+
+}
