ff6fc497fd9d2c610faf09733a8791b9a67b5d2d
Yev Bronshteyn <yebronsh@microsoft.com>
2020-07-21 22:56:03 -0400

    Adding blob storage actuator integration

---
+++ spring-cloud-azure-storage/src/main/java/com/microsoft/azure/spring/cloud/autoconfigure/storage/actuator/AzureStorageActuatorConstants.java
@@ -0,0 +1,11 @@
+package com.microsoft.azure.spring.cloud.autoconfigure.storage.actuator;
+
+import java.time.Duration;
+
+import org.springframework.boot.actuate.health.Status;
+
+public final class AzureStorageActuatorConstants {
+    static final String URL_FIELD = "URL";
+    static final Duration POLL_TIMEOUT = Duration.ofSeconds(2);
+    static final Status NOT_CONFIGURED_STATUS = new Status("Not configured");
+}
